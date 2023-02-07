cf2c4404ca229649f6f97357e4fde7c174f99a48
Haneenghassan <shratehhaneen@gmail.com>
2023-02-07 22:41:23 +0000

    task2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './Header.js';
+import Content from './Content';
+import Footer from './Footer.js';
+import React ,{Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+// function App() {
+//   return (
+//     <div className="App">
+//      hello react
+//     </div>
+//   );
+// }
+class App extends Component {
+  
+
+
+  render() {
+    return (
+      <>
+    <Header/>
+    <Content/>
+    <Footer/>
+    </>
+    )
+  }
+}
 export default App;
